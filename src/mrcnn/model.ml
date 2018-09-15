3f19bbcc0173e90d6d55a77a8def168351f69f53
pvdhove <pierre.vdhove@gmail.com>
2018-09-15 11:44:23 +0100

    works with owl multioutput

--- src/mrcnn/model.ml
+++ src/mrcnn/model.ml
@@ -19,13 +19,13 @@ let mrcnn num_anchors =
     if C.image_shape.(0) mod 64 <> 0 || C.image_shape.(1) mod 64 <> 0 then
       invalid_arg "Image width and height must be divisible by 64" in
   (* compensates for the lack of Padding2D *)
-  let input_shape = [|C.image_shape.(0) + 6; C.image_shape.(1) + 6; 3|] in
-  let input_image = input ~name:"input_image" input_shape in
-
-  let input_image_meta =
-    input ~name:"input_image_meta" [|C.image_meta_size|] in
-  let anchors =
-    input ~name:"input_anchors" [|num_anchors|] in
+  let image_shape = [|C.image_shape.(0) + 6; C.image_shape.(1) + 6; 3|] in
+  let inputs = inputs
+                 ~names:[|"input_image"; "input_image_meta"; "input_anchors"|]
+                 [|image_shape; [|C.image_meta_size|]; [|num_anchors; 4|]|] in
+  let input_image = inputs.(0)
+  and input_image_meta = inputs.(1)
+  and input_anchors = inputs.(2) in
 
   let _, c2, c3, c4, c5 = Resnet.resnet101 input_image in
 
@@ -69,7 +69,7 @@ let mrcnn num_anchors =
   let rpn_rois =
     let prop_f = PL.proposal_layer C.post_nms_rois C.rpn_nms_threshold in
     lambda_array [|C.post_nms_rois; 4|] prop_f ~name:"ROI"
-      [|rpn_class; rpn_bbox; anchors|] in
+      [|rpn_class; rpn_bbox; input_anchors|] in
 
   let _, mrcnn_class, mrcnn_bbox =
     FPN.fpn_classifier_graph rpn_rois mrcnn_feature_maps input_image_meta
@@ -84,19 +84,10 @@ let mrcnn num_anchors =
 
   let mrcnn_mask = FPN.build_fpn_mask_graph detection_boxes mrcnn_feature_maps
                      input_image_meta C.mask_pool_size C.num_classes in
-  get_multi_network ~name:C.name
-    [|input_image; input_image_meta; anchors|]
-    [|detections; mrcnn_mask|]
+  outputs ~name:C.name [|detections; mrcnn_mask|]
 
 
 (* *** Input and Output Processing *** *)
-
-let get_output nn node_name =
-  let node = get_node nn node_name in
-  match node.output with
-  | Some x -> unpack_arr x
-  | None -> invalid_arg (node_name ^ " node outputs can't be found.")
-
 
 type results = {
     rois: N.arr;
@@ -117,8 +108,8 @@ let extract_features detections mrcnn_masks image_meta =
   { rois; class_ids; scores; masks; }
 
 let detect () =
-  let anchors = Image.get_anchors C.image_shape in
-  let nn = mrcnn (N.shape anchors).(0) in
+  let anchors = N.expand (Image.get_anchors C.image_shape) 3 in
+  let nn = mrcnn (N.shape anchors).(1) in
   Printf.printf "Graph built!\n%!";
   Load_weights.load nn;
   Printf.printf "Weights loaded!\n%!";
@@ -128,7 +119,9 @@ let detect () =
     (* quick hack to replace padding2d *)
     let image = N.pad ~v:0. [[3;3];[3;3];[0;0]] molded_image in
 
-    let outputs = Graph.multi_model [|image; image_meta; anchors|] nn in
+    let image = N.expand image 4 in
+    let image_meta = N.expand image_meta 2 in
+    let outputs = model_inputs nn [|image; image_meta; anchors|] in
     Printf.printf "Computation done!\n%!";
     (* N.print ~max_row:500 ~max_col:20 (get_output nn "mrcnn_class"); *)
     let results = extract_features outputs.(0) outputs.(1) image_meta in
